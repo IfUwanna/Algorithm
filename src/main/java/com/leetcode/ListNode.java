55010d4f21b2845196852bde57d859c2c2ae6e3c
Jihun Park <ongamenet87@gmail.com>
2022-01-10 22:39:41 +0900

    220110-2

--- src/main/java/com/leetcode/ListNode.java
+++ src/main/java/com/leetcode/ListNode.java
@@ -14,7 +14,7 @@ package com.leetcode;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
--- src/main/java/com/leetcode/medium/RemoveNodeOfLinkedList.java
+++ src/main/java/com/leetcode/medium/RemoveNodeOfLinkedList.java
@@ -41,12 +41,29 @@ public class RemoveNodeOfLinkedList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
 
-        //2. length 체크 후 순회
-        // List length check
+        //1. 2개의 포인터 갭을 이용
+//        ListNode dummyHead = new ListNode(0);
+//        dummyHead.next = head;
+//        ListNode slow = dummyHead;
+//        ListNode fast = dummyHead;
+//
+//        while(n>0){ //n만큼 두 포인터를 벌려줌
+//            fast = fast.next;
+//            n--;
+//        }
+//        while(fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+//        slow.next = slow.next.next;
+//        return dummyHead.next;
+
+        //2. length 체크 + 배열 이용 - 시간복잡도(N), 공간복잡도 O(N)
+        ListNode[] a = new ListNode[30];
         int len = 0;
         ListNode node = head;
-        while(node!=null){
-            len++;
+        while(node!=null){ // i= index+1 (length)
+            a[len++] = node;
             node=node.next;
         }
 
@@ -55,19 +72,35 @@ public class RemoveNodeOfLinkedList {
         } else if(len-n == 0){ //  제거노드가 맨 앞이 되는 케이스
             head = head.next;
         } else{
-            node = head;
-            for (int i = 1; i <= len-n; i++) {  //제거 기준 노드 이전 노드 까지만 반복  [1,2,3,4,5] 4제거시 3까지만.
-                if(i==len-n){ //제거 노드 이전노드 도착
-                    if(node.next == null){  // 마지막 노드 제거 케이스
-                        head = null;
-                    }else{
-                        node.next = node.next.next;
-                    }
-                }else{
-                    node = node.next;
-                }
-            }
+            node =  a[len-n-1];  //제거 대상 이전 노드
+            node.next = node.next.next;
         }
         return head;
+
+
+        //2. 1. length 체크 + 순회 - 시간복잡도O(N)+@, 공간복잡도 O(1)
+        // List length check
+//        int len = 0;
+//        ListNode node = head;
+//        while(node!=null){
+//            len++;
+//            node=node.next;
+//        }
+//
+//        if(len == 1){  // 리스트 길이가 1개인 경우 무조건 제거
+//            head = null;
+//        } else if(len-n == 0){ //  제거노드가 맨 앞이 되는 케이스
+//            head = head.next;
+//        } else{
+//            node = head;
+//            for (int i = 1; i <= len-n; i++) {  //제거 기준 노드 이전 노드 까지만 반복  [1,2,3,4,5] 4제거시 3까지만.
+//                if(i==len-n){ //제거 노드 이전노드 도착
+//                    node.next = node.next.next;
+//                }else{
+//                    node = node.next;
+//                }
+//            }
+//        }
+//        return head;
     }
 }
