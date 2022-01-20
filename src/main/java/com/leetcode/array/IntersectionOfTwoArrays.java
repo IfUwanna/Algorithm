308f42e48169ed6a75dc406ba17de0062b3a478f
Jihun Park <ongamenet87@gmail.com>
2022-01-20 23:03:41 +0900

    220120-3

---
+++ src/main/java/com/leetcode/array/IntersectionOfTwoArrays.java
@@ -0,0 +1,67 @@
+package com.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * packageName    : com.leetcode.array
+ * fileName       : IntersectionOfTwoArrays
+ * author         : Jihun Park
+ * date           : 2022/01/20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/01/20        Jihun Park       최초 생성
+ */
+
+public class IntersectionOfTwoArrays {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        // 1. using array
+        int[] map = new int[1001]; //각 value를 index(key)로 갯수 저장 >> 1 <= nums1.length, nums2.length <= 1000
+        for(int num1 : nums1){
+            map[num1]++;
+        }
+        // check intersection
+        List<Integer> resultList = new ArrayList<>();
+        for(int num2 : nums2){
+            if(map[num2] > 0){ //교집합
+                resultList.add(num2);
+                map[num2]--;
+            }
+        }
+        // convert List to Array
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size() ; i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+    public int[] intersect2(int[] nums1, int[] nums2) {
+
+        // 2. using Map
+        Map<Integer,Integer> map = new HashMap<>(); //각 value를 index(key)로 갯수 저장
+        for(int num1 : nums1){
+            map.put(num1,map.getOrDefault(num1,0)+1);
+        }
+        // check intersection
+        List<Integer> resultList = new ArrayList<>();
+        for(int num2 : nums2){
+            if(map.getOrDefault(num2,0) > 0){ //교집합
+                resultList.add(num2);
+                map.put(num2,map.get(num2)-1);
+            }
+        }
+        // convert List to Array
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size() ; i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+
+    }
+}
---
+++ test/com/leetcode/array/IntersectionOfTwoArraysTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : com.leetcode.array
+ * fileName       : IntersectionOfTwoArraysTest
+ * author         : Jihun Park
+ * date           : 2022/01/20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/01/20        Jihun Park       최초 생성
+ */
+class IntersectionOfTwoArraysTest {
+    IntersectionOfTwoArrays  intersectionOfTwoArrays = new  IntersectionOfTwoArrays();
+    @Test
+    void intersect() {
+        intersectionOfTwoArrays.intersect2(new int[]{1,2,2,1},new int[]{2,2});
+    }
+}
