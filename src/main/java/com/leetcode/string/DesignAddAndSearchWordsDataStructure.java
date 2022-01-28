65c29aafb2a32dd7660a827f96bd05500eba2ceb
Jihun Park <ongamenet87@gmail.com>
2022-01-28 13:54:49 +0900

    220128-1

---
+++ src/main/java/com/leetcode/string/DesignAddAndSearchWordsDataStructure.java
@@ -0,0 +1,28 @@
+package com.leetcode.string;
+
+/**
+ * packageName    : com.leetcode.string
+ * fileName       : DesignAddAndSearchWordsDataStructure
+ * author         : Jihun Park
+ * date           : 2022/01/28
+ * description    : 211. Design Add and Search Words Data Structure [Medium]
+ * https://leetcode.com/problems/design-add-and-search-words-data-structure/
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/01/28        Jihun Park       최초 생성
+ */
+public class DesignAddAndSearchWordsDataStructure {
+
+    public DesignAddAndSearchWordsDataStructure() {
+
+    }
+
+    public void addWord(String word) {
+
+    }
+
+    public boolean search(String word) {
+        return true;
+    }
+}
