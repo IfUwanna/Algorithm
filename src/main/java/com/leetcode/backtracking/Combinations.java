d1f3cc9eae2f24697a339fc80de569e4f887cb2a
Jihun Park <ongamenet87@gmail.com>
2022-02-02 15:57:31 +0900

    220202-2

--- src/main/java/com/leetcode/backtracking/Combinations.java
+++ src/main/java/com/leetcode/backtracking/Combinations.java
@@ -41,5 +41,47 @@ public class Combinations {
             comb.remove(comb.size() - 1); 
         }
     }
+
+    /**
+     * 재귀 사용
+     * nCr : n개의 항목에서 r개를 뽑는다.
+     * n : 전체 크기 혹은 길이
+     * r : 뽑을 갯수
+     * depth : 상태 공간 트리에서의 depth -> 현재 index를 의미
+     */
+    static void recursiveCombination(int[] arr, boolean[] visited, int depth, int n, int r) {
+        if (r == 0) {
+            //print(arr, visited, n);
+            return;
+        }
+        if (depth == n) {
+            return;
+        }
+
+        visited[depth] = true;
+        recursiveCombination(arr, visited, depth + 1, n, r - 1);
+        visited[depth] = false;
+        recursiveCombination(arr, visited, depth + 1, n, r);
+    }
+
+    /**
+     * 재귀 사용
+     * nCr : n개의 항목에서 r개를 뽑는다.
+     * n : 전체 크기 혹은 길이
+     * r : 뽑을 갯수
+     * depth : 상태 공간 트리에서의 depth -> 현재 index를 의미
+     */
+    static void backtrackingCombination(int[] arr, boolean[] visited, int depth, int n, int r) {
+        if (r == 0) {
+            //print(arr, visited, n);
+            return;
+        }
+
+        for (int i = depth; i < n; i++) {
+            visited[i] = true;
+            backtrackingCombination(arr, visited, i + 1, n, r - 1);
+            visited[i] = false;
+        }
+    }
 }
 
