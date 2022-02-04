16fc5f821156608b5504f3d9d5501f70546ca028
Jihun Park <ongamenet87@gmail.com>
2022-02-04 14:17:45 +0900

    220204-4

--- src/main/java/com/leetcode/array/MergeSortedArray.java
+++ src/main/java/com/leetcode/array/MergeSortedArray.java
@@ -23,21 +23,35 @@ public class MergeSortedArray {
      * @param n
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int len = nums1.length;
-        int idx1 = m-1; // nums1 last value index
-        int idx2 = n-1; // nums2 last value index
-        for (int i = len-1; i >= 0 && idx1 >=0 && idx2 >=0; i--) { // nums1 끝에서부터 채워넣음
+        int len1 = nums1.length;
+        int idx1 = m-1;
+        int idx2 = n-1;
+        int idx = len1-1;
+        while(idx1 >= 0 && idx2 >= 0){ // 둘다 유효할 경우.
             if(nums1[idx1] >= nums2[idx2]){
-                nums1[i] = nums1[idx1--];
+                nums1[idx--] = nums1[idx1--];
             }else{
-                nums1[i] = nums2[idx2--];
+                nums1[idx--] = nums2[idx2--];
             }
         }
+        while(idx2 >= 0){nums1[idx--] = nums2[idx2--];}
 
-        while(idx2 >= 0){ // 배열2가 남으면 배열1에 순차적으로 채워줌
-            nums1[idx2] = nums2[idx2--];
-        }
+//        int len = nums1.length;
+//        int idx1 = m-1; // nums1 last value index
+//        int idx2 = n-1; // nums2 last value index
+//        for (int i = len-1; i >= 0 && idx1 >=0 && idx2 >=0; i--) { // nums1 끝에서부터 채워넣음
+//            if(nums1[idx1] >= nums2[idx2]){
+//                nums1[i] = nums1[idx1--];
+//            }else{
+//                nums1[i] = nums2[idx2--];
+//            }
+//        }
+//
+//        while(idx2 >= 0){ // 배열2가 남으면 배열1에 순차적으로 채워줌
+//            nums1[idx2] = nums2[idx2--];
+//        }
+
+
     }
 
 }
