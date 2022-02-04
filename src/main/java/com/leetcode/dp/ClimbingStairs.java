1ec7173e6567fa5569023ca007c5eb7d07839920
Jihun Park <ongamenet87@gmail.com>
2022-02-05 00:09:56 +0900

    220205-1

---
+++ src/main/java/com/leetcode/dp/ClimbingStairs.java
@@ -0,0 +1,88 @@
+package com.leetcode.dp;
+
+/**
+ * packageName    : com.leetcode.dp
+ * fileName       : ClimbingStairs
+ * author         : Jihun Park
+ * date           : 2022/02/04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/02/04        Jihun Park       최초 생성
+ */
+public class ClimbingStairs {
+    /**
+     * methodName : 70. Climbing Stairs
+     * author : Jihun Park
+     * description : https://leetcode.com/problems/climbing-stairs/
+     * @param n
+     * @return int
+     */
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+
+    public int climbStairs2(int n){
+        int[] dp = new int[n + 1];
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i - 2];
+        }
+        return dp[n];
+
+    }
+    public int climbStairs3(int n) {
+        // base cases
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for(int i=2; i<n; i++){
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
+
+    class Solution {
+        public int climbStairs(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return 2;
+            }
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+    }
+}
