61460ce4cf6f6865fbce3934860b5bf6130f4fd9
Jihun Park <ongamenet87@gmail.com>
2022-02-20 00:05:22 +0900

    220219

--- src/main/java/com/codility/lesson15/caterpillarmethod/AbsDistinct.java
+++ src/main/java/com/codility/lesson15/caterpillarmethod/AbsDistinct.java
@@ -12,5 +12,17 @@ package com.codility.lesson15.caterpillarmethod;
  * 2022/02/19        Jihun Park       최초 생성
  */
 public class AbsDistinct {
+    public int solution(int[] A, int[] B) {
+        int len = A.length;
+        if(len == 0) return 0;
+        int cnt = 1;
+        int prev = B[0];
+        for(int i=1; i<len; i++){
+            if(A[i] <= prev)continue;
+            prev = B[i];
+            cnt++;
+        }
+        return cnt;
+    }
 
 }
