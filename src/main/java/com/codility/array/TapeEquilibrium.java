485e78d700f5f2d07fbed970781efaa7d8f2ccf8
Jihun Park <ongamenet87@gmail.com>
2022-02-19 19:35:48 +0900

    220219-3

--- src/main/java/com/codility/array/TapeEquilibrium.java
+++ src/main/java/com/codility/array/TapeEquilibrium.java
@@ -12,5 +12,20 @@ package com.codility.array;
  * 2022/02/19        Jihun Park       최초 생성
  */
 public class TapeEquilibrium {
+    public static int solution(int[] A) {
+        int len = A.length;
+        int result = Integer.MAX_VALUE;
+        int[] dp = new int[len];
+        dp[0] = A[0];
+        for(int i=1; i < len; i++){
+            dp[i] = dp[i-1] + A[i];
+        }
 
+        for(int i=0; i < len-1; i++){
+            int difference = Math.abs(dp[i]-(dp[len-1] - dp[i]));
+            result = Math.min(difference,result);
+        }
+
+        return result;
+    }
 }
---
+++ test/com/codility/array/TapeEquilibriumTest.java
@@ -0,0 +1,24 @@
+package com.codility.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : com.codility.array
+ * fileName       : TapeEquilibriumTest
+ * author         : Jihun Park
+ * date           : 2022/02/19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/02/19        Jihun Park       최초 생성
+ */
+class TapeEquilibriumTest {
+
+    @Test
+    void solution() {
+        TapeEquilibrium.solution(new int[]{0,2000});
+    }
+}
