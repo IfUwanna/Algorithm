6f7712bfcd04a49fb9a63029455cf182e5dbb8b8
Jihun Park <ongamenet87@gmail.com>
2022-02-21 11:44:30 +0900

    220221-1

--- src/main/java/com/codility/lesson2/arrays/CyclicRotation.java
+++ src/main/java/com/codility/lesson2/arrays/CyclicRotation.java
@@ -25,7 +25,6 @@ public class CyclicRotation {
         int len = A.length;
         if(len == 0) return A;
         K = K%len;
-        //3. reverse 이용
         reverse(A,0,len-1); //step1 전체 reverse [7,6,5,4,3,2,1]
         reverse(A,0,K-1);   //step2 로테이션 숫자들 reverse [ 5,6,7,4,3,2,1 ]
         reverse(A,K,len-1);      //step3 나머지 숫자들 reverse [ 5,6,7,1,2,3,4 ]
